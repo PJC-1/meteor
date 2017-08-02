284782e130cd81b4778cb00a883df4128d8eafa5
PJC-1 <jumpzine@gmail.com>
2017-08-01 23:12:27 -0700

    reverted to 1.3.5.1 because of a bug with 1.5 meteor

--- textcircle.js
+++ textcircle.js
@@ -1,9 +1,26 @@
-if (Meteor.isClient){
+// creating a collection, using 'this' to give access to a package later
+this.Documents = new Mongo.Collection("documents");
 
+if (Meteor.isClient){
+		Template.editor.helpers({
+				docid:function(){
+						console.log("doc _id helper");
+						console.log(Documents.findOne());
+						var doc = Documents.findOne();
+						if (doc){
+								return doc._id;
+						} else {
+								return undefined;
+						}
+				}
+		});
 }
 
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
-	})
+		if (!Documents.findOne()){ //no documents yet!
+				Documents.insert({title:"my new document"});
+		}
+	});
 }
